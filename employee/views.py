801b9dcc5c54575cd907cf6877ac34d3b6c1bd4e
barai3045 <santosh.barai.045@gmail.com>
2024-09-17 17:30:40 +0600

    init

---
+++ employee/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    context = {
+        
+    }
+
+    return render(request, "home.html", context)
